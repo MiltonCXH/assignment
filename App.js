058dfd57da1d133acc2e6335628942249e9ac7f0
MiltonCXH <85888836+MiltonCXH@users.noreply.github.com>
2021-08-30 12:50:28 +0800

    working build

--- App.js
+++ App.js
@@ -7,16 +7,22 @@
  */
 
 import React, { Component } from 'react';
+import { Provider } from 'react-native-paper'
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { theme } from './src/core/theme'
+import {
+  StartScreen,
+  LoginScreen,
+  RegisterScreen,
+  ResetPasswordScreen,
+  Dashboard,
+} from './src/screens'
 
 import firebase from '@react-native-firebase/app';
 
-// TODO(you): import any additional firebase services that you require for your app, e.g for auth:
-//    1) install the npm package: `yarn add @react-native-firebase/auth@alpha` - you do not need to
-//       run linking commands - this happens automatically at build time now
-//    2) rebuild your app via `yarn run run:android` or `yarn run run:ios`
-//    3) import the package here in your JavaScript code: `import '@react-native-firebase/auth';`
-//    4) The Firebase Auth service is now available to use here: `firebase.auth().currentUser`
+const Stack = createStackNavigator()
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
@@ -30,19 +36,54 @@ const firebaseCredentials = Platform.select({
 
 type Props = {};
 
+const tag = "FIREBASE";
 export default class App extends Component<Props> {
+  state = {
+    isLogin: false,
+    authenticated: false
+  }
+
+  componentDidMount() {
+    this.isTheUserAuthenticated();
+  }
+
+  isTheUserAuthenticated = () => {
+    let user = firebase.auth().currentUser;
+    if (user) {
+      console.log(tag, user);
+
+      this.setState({ authenticated: true });
+    } else {
+      this.setState({ authenticated: false });
+    }
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native + Firebase!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        {!firebase.apps.length && (
-          <Text style={styles.instructions}>
-            {`\nYou currently have no Firebase apps registered, this most likely means you've not downloaded your project credentials. Visit the link below to learn more. \n\n ${firebaseCredentials}`}
-          </Text>
-        )}
-      </View>
+      <Provider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="StartScreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="StartScreen" component={StartScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+          <Stack.Screen
+            name="ResetPasswordScreen"
+            component={ResetPasswordScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+      {!firebase.apps.length && (
+        <Text style={styles.instructions}>
+          {`\nYou currently have no Firebase apps registered, this most likely means you've not downloaded your project credentials. Visit the link below to learn more. \n\n ${firebaseCredentials}`}
+        </Text>
+      )}
+    </Provider>
     );
   }
 }
---
+++ src/screens/Dashboard.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Paragraph from '../components/Paragraph'
+import Button from '../components/Button'
+import auth, { firebase } from "@react-native-firebase/auth"
+
+
+
+export default function Dashboard({ navigation }) {
+  return (
+    <Background>
+      <Logo />
+      <Header>Let’s start</Header>
+      <Paragraph>
+        Your amazing app starts here. Open you favorite code editor and start
+        editing this project.
+      </Paragraph>
+      <Button
+        mode="outlined"
+        onPress={async () => {
+            await firebase.auth().signOut();
+          },
+          navigation.reset({
+            index: 0,
+            routes: [{ name: 'StartScreen' }],
+          })
+        }
+      >
+        Logout
+      </Button>
+    </Background>
+  )
+}
